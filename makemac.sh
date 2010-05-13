a58603e69d2a0aad2023f1b670c72d8513e77077
Ryan C. Gordon <icculus@icculus.org>
2010-05-13 00:25:48 -0400

    Fixed makemac.sh

--- makemac.sh
+++ makemac.sh
@@ -9,10 +9,10 @@ for arch in ppc i386 x86_64 ; do
     make -f makefile.old macosx=true macosx_arch=$arch clean
     make -f makefile.old macosx=true macosx_arch=$arch -j$NCPU
     # We always strip here. For debugging, you should do "make" directly.
-    strip Data/lugaru-$arch
-    BINS="$BINS Data/lugaru-$arch"
+    strip ./lugaru-$arch
+    BINS="$BINS ./lugaru-$arch"
 done
 
-rm -f Data/lugaru-bin
-lipo -create -o Data/lugaru-bin $BINS
+rm -f ./lugaru-bin
+lipo -create -o ./lugaru-bin $BINS
 
