0cf95a37b66deb42a83a50caad6befea0d345bdf
Neal Gompa <ngompa13@gmail.com>
2010-05-12 19:38:56 -0500

    Disabled inclusion of 'vld.h' in 'OpenGL_Windows.cpp' because we don't need Visual Leak Detector

--- Source/OpenGL_Windows.cpp
+++ Source/OpenGL_Windows.cpp
@@ -19,10 +19,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-
+/* No need for Visual Leak Detector
 #ifdef WIN32
 #include <vld.h>
 #endif
+*/
 
 #ifndef USE_DEVIL
 #  ifdef WIN32
