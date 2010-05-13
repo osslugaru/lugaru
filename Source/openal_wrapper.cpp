abf8445c6bae263c5a931e3c5693b7879e19df8e
Neal Gompa <ngompa13@gmail.com>
2010-05-13 03:14:35 -0500

    Made fixes for includes in 'openal_wrapper.cpp' and tweaked the CMake scripts a bit

--- Source/openal_wrapper.cpp
+++ Source/openal_wrapper.cpp
@@ -33,8 +33,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "MacCompatibility.h"
 #include "fmod.h"
 
-#include "AL/al.h"
-#include "AL/alc.h"
+#include "al.h"
+#include "alc.h"
 
 #include "ogg/ogg.h"
 #include "vorbis/vorbisfile.h"
