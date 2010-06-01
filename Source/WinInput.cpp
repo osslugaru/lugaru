4ec0f8c7dd3faf7878b1bb460a381ebe98848e6c
Neal Gompa <ngompa13@gmail.com>
2010-06-01 05:07:03 -0500

    Fixed 'string.h' include to make cross compiling work from Linux to Windows

--- Source/WinInput.cpp
+++ Source/WinInput.cpp
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "WinInput.h"
 
 #ifdef WIN32
-#include "String.h"
+#include <string.h>
 #else
 #include <stdio.h>
 #include <stdlib.h>
