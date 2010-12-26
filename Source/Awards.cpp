da075483fcd3f0bdb7319a82567217e622f80c23
Alexander Monakov <amonakov@gmail.com>
2010-12-26 13:12:32 +0300

    Add a FIXME reminder

--- Source/Awards.cpp
+++ Source/Awards.cpp
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "Person.h"
 #include "Game.h"
 
+// FIXME: make these per-player
 float damagetaken;
 int numfalls;
 int numflipfail;
