4f6fe7f0aa492634a219ae0f194d6640ec3da429
Alexander Monakov <amonakov@gmail.com>
2011-01-01 22:52:07 +0300

    Drop useless ATM zero-initialization

--- Source/Person.h
+++ Source/Person.h
@@ -48,12 +48,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define rabbittype 0
 #define wolftype 1
 
-class Person
+struct Person
 {
-	public:
-		Person();
-		~Person();
-
 		int whichpatchx;
 		int whichpatchz;
 		
